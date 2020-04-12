a739892c27d65357f69edf3e51e2ec99802e7470
hendra <hendragebrian10@gmail.com>
2020-04-12 23:30:57 +0700

    Initial Commit

---
+++ Swift Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Learning
+//
+//  Created by hendra febrian on 12/04/20.
+//  Copyright © 2020 hendra febrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
